4c218a9ca333809b3e92ae078e9c72d8e484b050
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-09-12 18:55:02 +0200

    Add event listener to set font size of the sample text.

--- 01 project - fontChanger/script.js
+++ 01 project - fontChanger/script.js
@@ -5,3 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const range = inputRange.value;
   sampleText.style.fontSize = `${range}px`;
 });
+
+inputRange.addEventListener('input', function () {
+  // this => this is bing to the input in this case
+  const range = this.value;
+  sampleText.style.fontSize = `${range}px`;
+});
