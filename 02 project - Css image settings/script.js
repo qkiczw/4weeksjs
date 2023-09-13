48fa27a70a5f43319c1efcf14b81646da78bdf19
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-09-13 18:35:01 +0200

    Add code to change css filter properties on the sample image.

--- 02 project - Css image settings/script.js
+++ 02 project - Css image settings/script.js
@@ -3,12 +3,17 @@ const sampleImage = document.querySelector('.sample-image');
 const brightnessBar = document.querySelector('.brightness-bar');
 const contrastBar = document.querySelector('.contrast-bar');
 const saturationBar = document.querySelector('.saturation-bar');
+const hueBar = document.querySelector('.hue-bar');
 const blurBar = document.querySelector('.blur-bar');
 
-const bars = [brightnessBar, contrastBar, saturationBar, blurBar];
+const bars = [brightnessBar, contrastBar, saturationBar, hueBar, blurBar];
 
 bars.forEach((bar) => {
   bar.addEventListener('input', function () {
-    console.log(bar.value);
+    root.style.setProperty('--brightness', `${brightnessBar.value}%`);
+    root.style.setProperty('--contrast', `${contrastBar.value}%`);
+    root.style.setProperty('--saturation', `${saturationBar.value}%`);
+    root.style.setProperty('--hue', `${hueBar.value}deg`);
+    root.style.setProperty('--blur', `${blurBar.value}px`);
   });
 });
