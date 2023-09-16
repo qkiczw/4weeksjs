ed13b05b6239d1ae7fe2a9ae2180e06f1ee7272d
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-09-16 19:05:25 +0200

    Add function to create simple li item with note in it.

--- 03 project - ToDo List/script.js
+++ 03 project - ToDo List/script.js
@@ -1,18 +1,25 @@
 console.log(`Hello World`);
 // - funtion to create notes
-// - funtion to delete notes
+// - funtion to delete notes (use .remove() on element to delete!)
 // - function to save notes
 // - function init to read notes form the localsorage
 
 const AddNoteBtn = document.querySelector('.add-note-btn');
-const noteText = document.querySelector('.note-input').value;
 
-const saveNote = function () {
+const saveNote = (note) => {
   const noteID = Date.now().toString();
-  const noteText = document.querySelector('.note-input').value;
-  localStorage.setItem(noteID, noteText);
+  localStorage.setItem(noteID, note);
 };
 
-AddNoteBtn.addEventListener('click', function () {
-  saveNote();
-});
+const createNote = () => {
+  const notesList = document.querySelector('.notes-list');
+  const noteText = document.querySelector('.note-input').value;
+  const liElement = document.createElement('li');
+
+  saveNote(noteText);
+
+  liElement.textContent = noteText;
+  notesList.appendChild(liElement);
+};
+
+AddNoteBtn.addEventListener('click', createNote);
