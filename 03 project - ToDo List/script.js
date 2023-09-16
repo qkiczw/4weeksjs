2ba4b8573b6ada1d17e0ab8105d69b61a4f9b2da
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-09-16 18:54:53 +0200

    Add function to store a note in a local storage.

--- 03 project - ToDo List/script.js
+++ 03 project - ToDo List/script.js
@@ -3,3 +3,16 @@ console.log(`Hello World`);
 // - funtion to delete notes
 // - function to save notes
 // - function init to read notes form the localsorage
+
+const AddNoteBtn = document.querySelector('.add-note-btn');
+const noteText = document.querySelector('.note-input').value;
+
+const saveNote = function () {
+  const noteID = Date.now().toString();
+  const noteText = document.querySelector('.note-input').value;
+  localStorage.setItem(noteID, noteText);
+};
+
+AddNoteBtn.addEventListener('click', function () {
+  saveNote();
+});
