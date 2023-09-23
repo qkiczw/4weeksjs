a4f9409e268da4ca319a1a627a0e7a9ab471a03f
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-09-23 17:54:50 +0200

    Add some code to display dummy results.

--- 04 poject - VotingApp/script.js
+++ 04 poject - VotingApp/script.js
@@ -1,1 +1,36 @@
+const votingForm = document.querySelector('.voting-form');
+const votingBtn = document.querySelector('.voting-btn');
+const userName = document.querySelector('.user-name');
+const userID = document.querySelector('.user-id');
+const options = document.querySelector('.options-input');
+const resultsList = document.querySelector('.voting-results');
 
+const voters = new Map([
+  ['Łukasz', ['1234']],
+  ['Maciej', ['4321']],
+  ['Martyna', ['4321']],
+  ['Milan', ['1111']],
+  ['Sara', ['0001']],
+]);
+
+const votesCast = new Map([
+  ['Opcja-01', 0],
+  ['Opcja-02', 0],
+  ['Opcja-03', 0],
+]);
+
+const displayResults = () => {
+  for (const [option, vote] of votesCast) {
+    console.log(option, vote);
+
+    resultsList.innerHTML += `<li>${option} - głosów oddanych: ${vote}</li>`;
+  }
+};
+
+votingForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(options.value);
+
+  resultsList.innerHTML = ``; // clear votes list
+  displayResults();
+});
