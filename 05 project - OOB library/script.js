8236ad4a77327c79eb1637c39a5b799d5c471b55
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-09-24 19:11:24 +0200

    Add class named Book to create books.

--- 05 project - OOB library/script.js
+++ 05 project - OOB library/script.js
@@ -1,1 +1,21 @@
+class Book {
+  #title;
+  #author;
+  #year;
 
+  constructor(title, author, year) {
+    this.#title = title;
+    this.#author = author;
+    this.#year = year;
+  }
+
+  showDetails() {
+    return `${this.#title}, ${this.#author}, ${this.#year}`;
+  }
+}
+
+class Library {}
+
+// const book = new Book('książka', 'me', '2022');
+// document.querySelector('.results').innerHTML = book.showDetails();
+// console.log(book.showDetails());
