3acffd04e4d996921e76a25eb2429de59ade4ea5
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-09-26 18:40:52 +0200

    Add method to add a book to a book collection.

--- 05 project - OOB library/script.js
+++ 05 project - OOB library/script.js
@@ -1,3 +1,5 @@
+const AddBookBtn = document.querySelector('.add-book-btn');
+
 class Book {
   #title;
   #author;
@@ -14,8 +16,39 @@ class Book {
   }
 }
 
-class Library {}
+class Library {
+  #bookCollection;
+
+  constructor() {
+    this.#bookCollection = new Map();
+  }
+
+  init() {
+    console.log('run with init()');
+  }
+
+  addBook() {
+    const title = document.querySelector('.add-book-title-input');
+    const author = document.querySelector('.add-book-author-input');
+    const year = document.querySelector('.add-book-year-input');
+
+    const book = new Book(title, author, year);
+    this.#bookCollection.set([title, book]);
+
+    document.querySelector('.results').innerHTML = `Ksiażka dodana`;
+    console.log(this.#bookCollection);
+  }
+}
+
+const biblioteka = new Library();
+
+biblioteka.init();
+
+AddBookBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  biblioteka.addBook();
+});
 
 // const book = new Book('książka', 'me', '2022');
 // document.querySelector('.results').innerHTML = book.showDetails();
-// console.log(book.showDetails());
+// console.log(book);
