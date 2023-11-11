bc9bfef1f162ff8c8bbee25bd1f19e5073832c75
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-11-11 13:00:30 +0100

    Add some code.

--- 07 project - Drawing app/script.js
+++ 07 project - Drawing app/script.js
@@ -1,13 +1,39 @@
 class DrawingApp {
   constructor() {
     this.canvas = document.getElementById('canvas');
+    this.painting = false;
     this.#init();
+    this.#initEvents();
   }
-
+  // #methods are launched from inside the class
   #init() {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.ctx = this.canvas.getContext('2d');
+
+    this.strokeStyle = 'white'; // colour of a pen
+  }
+  #initEvents() {
+    // TODO:mouse methods => mouse up, down and move
+    // TODO: touch methods (mobile phones) => touch start, end, move
+  }
+
+  // cursor position
+  #starPosition() {
+    this.painting = true;
+    this.#draw();
+  }
+  #endPosition() {
+    this.painting.false;
+  }
+
+  #draw() {
+    // TODO: draw method
+  }
+
+  changeColor(color) {
+    console.log(color);
+    this.ctx.strokeStyle(color);
   }
 }
 
