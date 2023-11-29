a309871a0d1ac07885f4238d4430039d86f878ca
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-11-29 17:57:59 +0100

    Add some code and rename images.

--- 09 project Memory game OOP/script.js
+++ 09 project Memory game OOP/script.js
@@ -1,13 +1,13 @@
 const images = [
-  'images/img01',
-  'images/img02',
-  'images/img03',
-  'images/img04',
-  'images/img05',
-  'images/img06',
-  'images/img07',
-  'images/img08',
-  'images/img09',
+  'images/img01.jpg',
+  'images/img02.jpg',
+  'images/img03.jpg',
+  'images/img04.jpg',
+  'images/img05.jpg',
+  'images/img06.jpg',
+  'images/img07.jpg',
+  'images/img08.jpg',
+  'images/img09.jpg',
 ];
 
 class Card {
@@ -16,19 +16,19 @@ class Card {
     this.mathed = false;
     this.element = document.createElement('div');
     this.element.classList.add('card');
+    this.element.style.backgroundImage = `url(${this.image})`;
     this.element.addEventListener('click', this.reveal.bind(this));
   }
-
   reveal() {
-    if (!this.mathed && Gamepad.revealedCards.length < 2) {
+    if (!this.mathed && Game.revealedCards.length < 2) {
       this.element.style.backgroundImage = `url(${this.image})`;
       this.element.classList.add('revealed');
       Game.revealedCards.push(this);
-    }
-    if (Game.revealedCards / length === 2) {
-      setTimeout(() => {
-        Game.checkMatch();
-      }, 500);
+      if (Game.revealedCards.length === 2) {
+        setTimeout(() => {
+          Game.checkMatch();
+        }, 500);
+      }
     }
   }
 
@@ -53,8 +53,8 @@ class Game {
     this.startTime = Date.now();
     this.interval = setInterval(() => {
       document.getElementById('time').textContent = (
-        Date.now() -
-        this.startTime / 1000
+        (Date.now() - this.startTime) /
+        1000
       ).toFixed(2);
     }, 100);
 
@@ -63,6 +63,7 @@ class Game {
     const cards = images.concat(images).map((img) => new Card(img));
     this.shuffle(cards).forEach((card) => board.appendChild(card.element));
 
+    // Hiding cards after a setted time
     setTimeout(() => {
       cards.forEach((card) => card.hide());
     }, 3000);
@@ -75,4 +76,27 @@ class Game {
     }
     return cards;
   }
+
+  static checkMatch() {
+    if (this.revealedCards[0].image === this.revealedCards[1].image) {
+      this.revealedCards[0].matchFound();
+      this.revealedCards[1].matchFound();
+      this.matchedPairs++;
+
+      if (this.matchedPairs === 9) {
+        clearInterval(this.interval);
+        alert(
+          `Gratulacje!!! Twój czas to ${
+            document.getElementById('time').textContent
+          } sekund.`
+        );
+      }
+    } else {
+      this.revealedCards[0].hide();
+      this.revealedCards[1].hide();
+    }
+    this.revealedCards = [];
+  }
 }
+
+Game.start(images);
